ed0835cccbb193acb906c340e8b3b9430b31a511
Thom Bakker <thombakker@gmail.com>
2018-10-23 15:29:06 +0200

    :fireworks: Add AStar

--- a1rpun-tick-tick-boom/Player.js
+++ a1rpun-tick-tick-boom/Player.js
@@ -1,46 +1,244 @@
 class Player {
-  constructor() {
-    this.previousHealth = 100;
-  }
-
-  playTurn(warrior) {
-    // Cool code goes here.
-    var health = warrior.health() / warrior.maxHealth() * 100;
-    var gotDamaged = health < this.previousHealth;
-    var senses = [
-      'backward',
-      'left',
-      'forward',
-      'right',
-    ];
-
-    var units = warrior.listen();
-var state = 'walk';
-var direction = warrior.directionOfStairs();
-for(let i = units.length;i--;){
-  var space = units[i];
-  var unit = space.getUnit();
-  var loc = space.getLocation();
-  var dir = warrior.directionOf(space);
-  var canInteract = (loc[0] === 0 && (loc[1] === -1 || loc[1] === 1))
-                    || (loc[1] === 0 && (loc[0] === -1 || loc[0] === 1));
-
-  if(unit.isUnderEffect('ticking')){
-    direction = dir;
-    if(canInteract){
-      state = 'rescue'
-      break;
+    constructor() {
+        this.finder = new AStar();
+        this.directions = {
+            backward: 'backward',
+            left: 'left',
+            forward: 'forward',
+            right: 'right',
+        };
     }
-  }
-}
-if(state === 'walk' && !warrior.feel(direction).isEmpty()){
-  direction = 'left';
-  if(!warrior.feel(direction).isEmpty()){
-    direction = 'forward';
-  }
-}
-warrior[state](direction);
 
-    this.previousHealth = health;
-  }
+    playTurn(warrior) {
+        // Cool code goes here.
+        var state = 'walk';
+        var direction = warrior.directionOfStairs();
+        var units = warrior.listen();
+        var space = units.find(x => x.getUnit().isUnderEffect('ticking'));
+        var rushing = !!space;
+
+        if (space) {
+            var grid = [];
+            var offsetX = 0;
+            var offsetY = 0;
+            var destX = 0;
+            var destY = 0;
+            // determine offset
+            for (let i = units.length; i--;) {
+                let loc = units[i].getLocation();
+                if (loc[0] < 0 && loc[0] * -1 > offsetX)
+                    offsetX = loc[0] * -1;
+                if (loc[1] < 0 && loc[1] * -1 > offsetY)
+                    offsetY = loc[1] * -1;
+            }
+            var width = 0;
+            var height = 0;
+            for (let i = units.length; i--;) {
+                let unit = units[i];
+                let loc = unit.getLocation();
+                let x = loc[0] + offsetX;
+                let y = loc[1] + offsetY;
+                if (x > width)
+                    width = x;
+                if (y > height)
+                    height = y;
+                if (!grid[y])
+                    grid[y] = [];
+                if (unit === space) {
+                    destX = x;
+                    destY = y;
+                }
+                grid[y][x] = unit.getUnit().isEnemy() ? 2 : 1;
+            }
+            for (let y = height + 1; y--;)
+                for (let x = width + 1; x--;)
+                    if (!grid[y][x])
+                        grid[y][x] = 0;
+
+            this.finder.setGrid(grid);
+            this.finder.setAcceptableTiles([0, 1]);
+            var path = this.finder.findPath([offsetX, offsetY], [destX, destY]);
+            if (path.length > 1) {
+                let dir = '';
+                let nextX = path[1][0];
+                let nextY = path[1][1];
+                if (nextX > offsetX)
+                    dir = 'forward';
+                else if (nextX < offsetX)
+                    dir = 'backward';
+                else if (nextY > offsetY)
+                    dir = 'right';
+                else
+                    dir = 'left';
+                space = warrior.feel(dir);
+            } else {
+                space = null;
+            }
+        }
+
+        if (!space) {
+            var closest = Infinity;
+            var closestIndex = -1;
+            for (let i = units.length; i--;) {
+                let closestSum = units[i].getLocation().reduce((a, b) => a + b, 0);
+                if (closestSum < closest || (closestSum === closest && units[closestIndex].getUnit().isBound())) {
+                    closestIndex = i;
+                }
+            }
+            if (~closestIndex) {
+                var s = units[closestIndex];
+                space = warrior.feel(warrior.directionOf(s));
+            }
+        }
+
+        if (space) {
+            direction = warrior.directionOf(space);
+            if (space.isUnit()) {
+                var unit = space.getUnit();
+                if (unit.isEnemy()) {
+                    state = rushing || unit.isBound() ? (false ? 'detonate' : 'attack') : 'bind';
+                } else {
+                    state = 'rescue';
+                }
+            } else if (!rushing && warrior.health() < 10) {
+                // TODO: heal to amount calculated on sum unit.health
+                state = 'rest';
+            }
+        }
+        warrior[state](direction);
+    }
+}
+
+var abs = Math.abs;
+
+class AStar {
+    constructor() {
+        this.acceptableTiles = [0];
+        this.grid = [[]];
+    }
+    setAcceptableTiles(acceptableTiles) {
+        this.acceptableTiles = acceptableTiles;
+    }
+    setGrid(level) {
+        this.grid = level;
+        // Note that this A-star implementation expects the world array to be square: 
+        // it must have equal height and width. If your game world is rectangular, 
+        // just fill the array with dummy values to pad the empty space.
+        this.worldHeight = level.length;
+        this.worldWidth = level[0].length;
+        this.worldSize = this.worldHeight * this.worldWidth;
+    }
+    ManhattanDistance(Point, Goal) {
+        return abs(Point.x - Goal.x) + abs(Point.y - Goal.y);
+    }
+    Neighbours(x, y) {
+        var N = y - 1,
+            S = y + 1,
+            E = x + 1,
+            W = x - 1,
+            myN = N > -1 && this.canWalkHere(x, N),
+            myS = S < this.worldHeight && this.canWalkHere(x, S),
+            myE = E < this.worldWidth && this.canWalkHere(E, y),
+            myW = W > -1 && this.canWalkHere(W, y),
+            result = [];
+
+        if (myN)
+            result.push({ x: x, y: N });
+        if (myE)
+            result.push({ x: E, y: y });
+        if (myS)
+            result.push({ x: x, y: S });
+        if (myW)
+            result.push({ x: W, y: y });
+        return result;
+    }
+    canWalkHere(x, y) {
+        return this.grid && this.grid[y] &&
+            this.acceptableTiles.indexOf(this.grid[y][x]) !== -1;
+    }
+    findPath(pathStart, pathEnd) {
+        var distanceFunction = this.ManhattanDistance;
+        var a = this;
+        function Node(Parent, Point) {
+            return {
+                // pointer to another Node object
+                Parent: Parent,
+                // array index of this Node in the world linear array
+                value: Point.x + (Point.y * a.worldWidth),
+                // the location coordinates of this Node
+                x: Point.x,
+                y: Point.y,
+                // the heuristic estimated cost
+                // of an entire path using this node
+                f: 0,
+                // the distanceFunction cost to get
+                // from the starting point to this node
+                g: 0
+            };
+        }
+        // create Nodes from the Start and End x,y coordinates
+        var mypathStart = Node(null, { x: pathStart[0], y: pathStart[1] });
+        var mypathEnd = Node(null, { x: pathEnd[0], y: pathEnd[1] });
+        // create an array that will contain all world cells
+        var AStar = [];
+        // list of currently open Nodes
+        var Open = [mypathStart];
+        // list of closed Nodes
+        var Closed = [];
+        // list of the final output array
+        var result = [];
+        // reference to a Node (that is nearby)
+        var myNeighbours;
+        // reference to a Node (that we are considering now)
+        var myNode;
+        // reference to a Node (that starts a path in question)
+        var myPath;
+        // temp integer variables used in the calculations
+        var length, max, min, i, j;
+        // iterate through the open list until none are left
+        while (length = Open.length) {
+            max = this.worldSize;
+            min = -1;
+            for (i = 0; i < length; i++) {
+                if (Open[i].f < max) {
+                    max = Open[i].f;
+                    min = i;
+                }
+            }
+            // grab the next node and remove it from Open array
+            myNode = Open.splice(min, 1)[0];
+            // is it the destination node?
+            if (myNode.value === mypathEnd.value) {
+                myPath = Closed[Closed.push(myNode) - 1];
+                do {
+                    result.push([myPath.x, myPath.y]);
+                }
+                while (myPath = myPath.Parent);
+                // clear the working arrays
+                AStar = Closed = Open = [];
+                // we want to return start to finish
+                result.reverse();
+            } else {// not the destination                
+                // find which nearby nodes are walkable
+                myNeighbours = this.Neighbours(myNode.x, myNode.y);
+                // test each one that hasn't been tried already
+                for (i = 0, j = myNeighbours.length; i < j; i++) {
+                    myPath = Node(myNode, myNeighbours[i]);
+                    if (!AStar[myPath.value]) {
+                        // estimated cost of this particular route so far
+                        myPath.g = myNode.g + distanceFunction(myNeighbours[i], myNode);
+                        // estimated cost of entire guessed route to the destination
+                        myPath.f = myPath.g + distanceFunction(myNeighbours[i], mypathEnd);
+                        // remember this new path for testing above
+                        Open.push(myPath);
+                        // mark this node in the world graph as visited
+                        AStar[myPath.value] = true;
+                    }
+                }
+                // remember this route as having no more untested options
+                Closed.push(myNode);
+            }
+        } // keep iterating until the Open list is empty
+        return result;
+    }
 }
--- a1rpun-tick-tick-boom/README.md
+++ a1rpun-tick-tick-boom/README.md
@@ -2,20 +2,20 @@
 
 ### _Try not to blow this tower apart_
 
-## Level 7
+## Level 9
 
-_Another ticking sound, but some sludge is blocking the way._
+_Never before have you seen a room so full of sludge. Start the fireworks!_
 
-> **TIP:** Quickly kill the sludge and rescue the captive before the bomb goes off. You can't simply go around them.
+> **TIP:** Be careful not to let the ticking captive get caught in the flames. Use `warrior.distanceOf()` to avoid the captives.
 
 ### Floor Map
 
 ```
-╔═════╗
-║ sC >║
-║@ C C║
-║ s   ║
-╚═════╝
+╔════╗
+║ssC>║
+║@sss║
+║ssC ║
+╚════╝
 
 s = Sludge (12 HP)
 C = Captive (1 HP)
@@ -25,22 +25,26 @@ C = Captive (1 HP)
 
 ## Abilities
 
+
 ### Actions (only one per turn)
 
+- `warrior.rest()`: Gains 10% of max health back, but does nothing more.
+- `warrior.detonate()`: Detonates a bomb in a given direction (`'forward'` by default), dealing 8 HP of damage to that space and 4 HP of damage to surrounding 4 spaces (including yourself).
 - `warrior.attack()`: Attacks a unit in the given direction (`'forward'` by default), dealing 5 HP of damage.
 - `warrior.bind()`: Binds a unit in the given direction (`'forward'` by default) to keep him from moving.
 - `warrior.rescue()`: Releases a unit from his chains in the given direction (`'forward'` by default).
-- `warrior.rest()`: Gains 10% of max health back, but does nothing more.
 - `warrior.walk()`: Moves one space in the given direction (`'forward'` by default).
 
 ### Senses
 
-- `warrior.maxHealth()`: Returns an integer representing your maximum health.
 - `warrior.directionOfStairs()`: Returns the direction (forward, right, backward or left) the stairs are from your location.
 - `warrior.directionOf()`: Returns the direction (forward, right, backward or left) to the given space.
+- `warrior.distanceOf()`: Returns an integer representing the distance to the given space.
 - `warrior.feel()`: Returns the adjacent space in the given direction (`'forward'` by default).
 - `warrior.health()`: Returns an integer representing your health.
 - `warrior.listen()`: Returns an array of all spaces which have units in them (excluding yourself).
+- `warrior.look()`: Returns an array of up to 3 spaces in the given direction (`'forward'` by default).
+- `warrior.maxHealth()`: Returns an integer representing your maximum health.
 - `warrior.think()`: Thinks out loud (`console.log` replacement).
 
 ## Next Steps
